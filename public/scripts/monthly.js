7f16616f1149126a8c08bf9dca48765671d91fb4
Kevin-Sarges <kevinsarges08@gmail.com>
2021-11-07 21:09:59 -0300

    Separendo as horas

--- public/scripts/monthly.js
+++ public/scripts/monthly.js
@@ -1,89 +1,91 @@
+monthly;
+
 $.ajax({
   url: "data.json",
   dataType: "json",
 
   success: function (data) {
-    const dailyWork = document.querySelector("#work");
-    const dailyPlay = document.querySelector("#play");
-    const dailyStudy = document.querySelector("#study");
-    const dailyExercise = document.querySelector("#exercise");
-    const dailySocial = document.querySelector("#social");
-    const dailySelf = document.querySelector("#self");
+    const monthlyWork = document.querySelector("#work");
+    const monthlyPlay = document.querySelector("#play");
+    const monthlyStudy = document.querySelector("#study");
+    const monthlyExercise = document.querySelector("#exercise");
+    const monthlySocial = document.querySelector("#social");
+    const monthlySelf = document.querySelector("#self");
 
-    dailyWork.innerHTML = `
+    monthlyWork.innerHTML = `
       <div class="hours">
         <div class="title">
           <h4>${data[0].title}</h4>
           <img src="./public/images/icon-ellipsis.svg" alt="options" />
         </div>
         <div class="text-hours">
-          <p id="time">${data[0].timeframes.daily.current}</p>
-          <p>Previous - ${data[0].timeframes.daily.previous}hrs</p>
+          <p id="time">${data[0].timeframes.monthly.current}</p>
+          <p>Previous - ${data[0].timeframes.monthly.previous}hrs</p>
         </div>
       </div>
     `;
 
-    dailyPlay.innerHTML = `
+    monthlyPlay.innerHTML = `
       <div class="hours">
         <div class="title">
           <h4>${data[1].title}</h4>
           <img src="./public/images/icon-ellipsis.svg" alt="options" />
         </div>
         <div class="text-hours">
-          <p id="time">${data[1].timeframes.daily.current}</p>
-          <p>Previous - ${data[1].timeframes.daily.previous}hrs</p>
+          <p id="time">${data[1].timeframes.monthly.current}</p>
+          <p>Previous - ${data[1].timeframes.monthly.previous}hrs</p>
         </div>
       </div>
     `;
 
-    dailyStudy.innerHTML = `
+    monthlyStudy.innerHTML = `
       <div class="hours">
         <div class="title">
           <h4>${data[2].title}</h4>
           <img src="./public/images/icon-ellipsis.svg" alt="options" />
         </div>
         <div class="text-hours">
-          <p id="time">${data[2].timeframes.daily.current}</p>
-          <p>Previous - ${data[2].timeframes.daily.previous}hrs</p>
+          <p id="time">${data[2].timeframes.monthly.current}</p>
+          <p>Previous - ${data[2].timeframes.monthly.previous}hrs</p>
         </div>
       </div>
     `;
 
-    dailyExercise.innerHTML = `
+    monthlyExercise.innerHTML = `
       <div class="hours">
         <div class="title">
           <h4>${data[3].title}</h4>
           <img src="./public/images/icon-ellipsis.svg" alt="options" />
         </div>
         <div class="text-hours">
-          <p id="time">${data[3].timeframes.daily.current}</p>
-          <p>Previous - ${data[3].timeframes.daily.previous}hrs</p>
+          <p id="time">${data[3].timeframes.monthly.current}</p>
+          <p>Previous - ${data[3].timeframes.monthly.previous}hrs</p>
         </div>
       </div>
     `;
 
-    dailySocial.innerHTML = `
+    monthlySocial.innerHTML = `
       <div class="hours">
         <div class="title">
           <h4>${data[4].title}</h4>
           <img src="./public/images/icon-ellipsis.svg" alt="options" />
         </div>
         <div class="text-hours">
-          <p id="time">${data[4].timeframes.daily.current}</p>
-          <p>Previous - ${data[4].timeframes.daily.previous}hrs</p>
+          <p id="time">${data[4].timeframes.monthly.current}</p>
+          <p>Previous - ${data[4].timeframes.monthly.previous}hrs</p>
         </div>
       </div>
     `;
 
-    dailySelf.innerHTML = `
+    monthlySelf.innerHTML = `
       <div class="hours">
         <div class="title">
           <h4>${data[5].title}</h4>
           <img src="./public/images/icon-ellipsis.svg" alt="options" />
         </div>
         <div class="text-hours">
-          <p id="time">${data[5].timeframes.daily.current}</p>
-          <p>Previous - ${data[5].timeframes.daily.previous}hrs</p>
+          <p id="time">${data[5].timeframes.monthly.current}</p>
+          <p>Previous - ${data[5].timeframes.monthly.previous}hrs</p>
         </div>
       </div>
     `;
--- public/scripts/weekly.js
+++ public/scripts/weekly.js
@@ -3,87 +3,87 @@ $.ajax({
   dataType: "json",
 
   success: function (data) {
-    const dailyWork = document.querySelector("#work");
-    const dailyPlay = document.querySelector("#play");
-    const dailyStudy = document.querySelector("#study");
-    const dailyExercise = document.querySelector("#exercise");
-    const dailySocial = document.querySelector("#social");
-    const dailySelf = document.querySelector("#self");
+    const weeklyWork = document.querySelector("#work");
+    const weeklyPlay = document.querySelector("#play");
+    const weeklyStudy = document.querySelector("#study");
+    const weeklyExercise = document.querySelector("#exercise");
+    const weeklySocial = document.querySelector("#social");
+    const weeklySelf = document.querySelector("#self");
 
-    dailyWork.innerHTML = `
+    weeklyWork.innerHTML = `
       <div class="hours">
         <div class="title">
           <h4>${data[0].title}</h4>
           <img src="./public/images/icon-ellipsis.svg" alt="options" />
         </div>
         <div class="text-hours">
-          <p id="time">${data[0].timeframes.daily.current}</p>
-          <p>Previous - ${data[0].timeframes.daily.previous}hrs</p>
+          <p id="time">${data[0].timeframes.weekly.current}</p>
+          <p>Previous - ${data[0].timeframes.weekly.previous}hrs</p>
         </div>
       </div>
     `;
 
-    dailyPlay.innerHTML = `
+    weeklyPlay.innerHTML = `
       <div class="hours">
         <div class="title">
           <h4>${data[1].title}</h4>
           <img src="./public/images/icon-ellipsis.svg" alt="options" />
         </div>
         <div class="text-hours">
-          <p id="time">${data[1].timeframes.daily.current}</p>
-          <p>Previous - ${data[1].timeframes.daily.previous}hrs</p>
+          <p id="time">${data[1].timeframes.weekly.current}</p>
+          <p>Previous - ${data[1].timeframes.weekly.previous}hrs</p>
         </div>
       </div>
     `;
 
-    dailyStudy.innerHTML = `
+    weeklyStudy.innerHTML = `
       <div class="hours">
         <div class="title">
           <h4>${data[2].title}</h4>
           <img src="./public/images/icon-ellipsis.svg" alt="options" />
         </div>
         <div class="text-hours">
-          <p id="time">${data[2].timeframes.daily.current}</p>
-          <p>Previous - ${data[2].timeframes.daily.previous}hrs</p>
+          <p id="time">${data[2].timeframes.weekly.current}</p>
+          <p>Previous - ${data[2].timeframes.weekly.previous}hrs</p>
         </div>
       </div>
     `;
 
-    dailyExercise.innerHTML = `
+    weeklyExercise.innerHTML = `
       <div class="hours">
         <div class="title">
           <h4>${data[3].title}</h4>
           <img src="./public/images/icon-ellipsis.svg" alt="options" />
         </div>
         <div class="text-hours">
-          <p id="time">${data[3].timeframes.daily.current}</p>
-          <p>Previous - ${data[3].timeframes.daily.previous}hrs</p>
+          <p id="time">${data[3].timeframes.weekly.current}</p>
+          <p>Previous - ${data[3].timeframes.weekly.previous}hrs</p>
         </div>
       </div>
     `;
 
-    dailySocial.innerHTML = `
+    weeklySocial.innerHTML = `
       <div class="hours">
         <div class="title">
           <h4>${data[4].title}</h4>
           <img src="./public/images/icon-ellipsis.svg" alt="options" />
         </div>
         <div class="text-hours">
-          <p id="time">${data[4].timeframes.daily.current}</p>
-          <p>Previous - ${data[4].timeframes.daily.previous}hrs</p>
+          <p id="time">${data[4].timeframes.weekly.current}</p>
+          <p>Previous - ${data[4].timeframes.weekly.previous}hrs</p>
         </div>
       </div>
     `;
 
-    dailySelf.innerHTML = `
+    weeklySelf.innerHTML = `
       <div class="hours">
         <div class="title">
           <h4>${data[5].title}</h4>
           <img src="./public/images/icon-ellipsis.svg" alt="options" />
         </div>
         <div class="text-hours">
-          <p id="time">${data[5].timeframes.daily.current}</p>
-          <p>Previous - ${data[5].timeframes.daily.previous}hrs</p>
+          <p id="time">${data[5].timeframes.weekly.current}</p>
+          <p>Previous - ${data[5].timeframes.weekly.previous}hrs</p>
         </div>
       </div>
     `;
